f729ce65fb4136fe1e26d62a04eb335aca496bb5
Chris HELLO <93587059+solidityFrance@users.noreply.github.com>
2023-09-13 13:32:47 +0200

    Add files via upload

---
+++ generators/ethereum.js
@@ -0,0 +1,39 @@
+var hdkey = require('ethereumjs-wallet/hdkey')
+var bip39 = require('bip39')
+var hdPathString = "m/44'/60'/0'/0"
+
+function generateEtherAccountsForMnemonic(mnemonic, accountCount) {
+
+  var real = mnemonic.trim().split(' ').map(function(word) {
+    return word.trim()
+  })
+  .filter((word) => {
+    return word !== ''
+  })
+  .join(' ')
+  console.dir({ real })
+  var seed = bip39.mnemonicToSeed(real)
+  var hdWallet = hdkey.fromMasterSeed(seed)
+  var root = hdWallet.derivePath(hdPathString)
+
+  var wallets = []
+  for (var i = 0; i < accountCount; i++) {
+    var child = root.deriveChild(i)
+    var wallet = child.getWallet()
+    wallets.push(wallet)
+  }
+
+  var hexWallets = wallets.map((w) => {
+    console.dir(w);
+    return {
+      address: w.getAddress().toString('hex'),
+      privKey: w.getPrivateKeyString(),
+    }
+  });
+
+  return hexWallets
+
+}
+
+module.exports = generateEtherAccountsForMnemonic
+
